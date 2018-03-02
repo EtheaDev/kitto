a7f360d530961b5c099f500acf991d738f816db0
Carlo Barazzetta <carlo.barazzetta@ethea.it>
2018-03-02 08:24:58 +0100

    +Updated DbDemos Example

---
+++ Examples/DbDemos/Source/UseKitto.pas
@@ -0,0 +1,26 @@
+unit UseKitto;
+
+interface
+
+uses
+  DBXMSSQL,
+  DBXFirebird,
+  EF.DB.FD, //FireDac support
+  // Kitto.AccessControl.DB,
+  // Kitto.Auth.DB,
+  // Kitto.Auth.DBServer,
+  // Kitto.Auth.OSDB,
+  Kitto.Auth.TextFile,
+  Kitto.Ext.ADOTools, //For Excel/Import export
+  Kitto.Ext.DebenuQuickPDFTools, //For PDF Merge
+  Kitto.Ext.FOPTools, //For FOP Engine
+  Kitto.Localization.dxgettext,
+  Kitto.Ext.TilePanel,
+  Kitto.Metadata.ModelImplementation,
+  Kitto.Metadata.ViewBuilders,
+  Kitto.Ext.All
+  ;
+
+implementation
+
+end.
